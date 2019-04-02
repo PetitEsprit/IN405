23b2b492fcb9cdc6293aa48fba9fca62790bbc2c
PetitEsprit <PetitEsprit@users.noreply.github.com>
2019-04-02 13:00:07 +0200

    bla

--- td6/ex03.c
+++ td6/ex03.c
@@ -3,24 +3,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct {
+	int nbThread;
+	int numThread;
+	pthread_mutex_t mut;
+	pthread_cond_t cond;
+} arg_t;
 
 void *ft_thread_gen(void *arg)
 {
-	int r = rand()%100 + 1;
-	printf("GENERATION FAITE : %d\n", r);
-	return r;
+	arg_t *ptrarg = (arg_t*)arg;
+	pthread_mutex_lock(&(ptrbar->mut));
+	ptrarg->nbIn = rand()%100 + 1;
+	pthread_mutex_unlock(&(ptrbar->mut));
+	printf("GENERATION FAITE\n");
+	return NULL;
 }
 
 void *ft_thread_transmit(void *arg)
 {
-	int n = (int)arg;
-	return n;
+	arg_t *ptrarg = (arg_t*)arg;
+	return ptrarg->;
 }
 
 void *ft_thread_end(void *arg)
 {
-	int n = (int)arg;
-	printf("MESSAGE : %d\n", n); 
+	arg_t *ptrarg = (arg_t*)arg;
+	printf("MASSAGE: %d");
 }
 
 
@@ -29,6 +38,14 @@ int main(int argc, char **argv)
 	if(argc > 1)
 	{
 		int i;
+		arg_t arg;
+		nbThreads = atoi(argv[1]);
+		pthread_t id[nbThreads];
+		pthread_mutex_t mut[nbThreads];
+		pthread_cond_t cond[nbThreads];
+		arg.nbFinish = 0;
+		pthread_mutex_init(&arg.mut, NULL);
+		pthread_cond_init(&arg.cond, NULL);
 		
 		srand(getpid());
 		pthread_create(id, NULL, ft_thread, &arg);
@@ -41,7 +58,7 @@ int main(int argc, char **argv)
 		}
 		
 		pthread_create(id+nbThreads-1, NULL, ft_thread, &arg);
-		pthread_join(id[nbThreads-1], NULL);
+		pthread_join(id[nbThreads-1], NULL);	
 		return 0;
 	}
 }
