6e23a7f8ec4feae19b1a303fab65c689d1b56cad
PetitEsprit <PetitEsprit@users.noreply.github.com>
2019-04-09 12:36:17 +0200

    fin ex03.c

--- td6/ex03.c
+++ td6/ex03.c
@@ -4,32 +4,52 @@
 #include <unistd.h>
 
 typedef struct {
-	int nbThread;
 	int numThread;
-	pthread_mutex_t mut;
-	pthread_cond_t cond;
+	int *in;
+	int *out;
+	pthread_mutex_t *mutin;
+	pthread_mutex_t *mutout;
+	pthread_cond_t *condin;
+	pthread_cond_t *condout;
 } arg_t;
 
-void *ft_thread_gen(void *arg)
+void *ft_thread_first(void *arg)
 {
 	arg_t *ptrarg = (arg_t*)arg;
-	pthread_mutex_lock(&(ptrbar->mut));
-	ptrarg->nbIn = rand()%100 + 1;
-	pthread_mutex_unlock(&(ptrbar->mut));
-	printf("GENERATION FAITE\n");
+	pthread_mutex_lock(ptrarg->mutout);
+	
+	*(ptrarg->out) = rand()%100 + 1;
+	printf("GENERATION FAITE: %d\n", *(ptrarg->out));
+	pthread_cond_signal(ptrarg->condout);
+	pthread_mutex_unlock(ptrarg->mutout);
+	
 	return NULL;
 }
 
-void *ft_thread_transmit(void *arg)
+void *ft_thread_mid(void *arg)
 {
 	arg_t *ptrarg = (arg_t*)arg;
-	return ptrarg->;
+	pthread_mutex_lock(ptrarg->mutin);
+	pthread_cond_wait(ptrarg->condin, ptrarg->mutin);
+	int tmp = *(ptrarg->in);
+	pthread_mutex_unlock(ptrarg->mutin);
+	
+	pthread_mutex_lock(ptrarg->mutout);
+	*(ptrarg->out) = tmp;
+	pthread_cond_signal(ptrarg->condout);
+	pthread_mutex_unlock(ptrarg->mutout);
+	printf("TRANSMITION FAITE\n");
+	return NULL;
 }
 
 void *ft_thread_end(void *arg)
 {
 	arg_t *ptrarg = (arg_t*)arg;
-	printf("MASSAGE: %d");
+	pthread_mutex_lock(ptrarg->mutin);
+	pthread_cond_wait(ptrarg->condin, ptrarg->mutin);
+	printf("FIN - MESSAGE: %d\n", *(ptrarg->in));
+	pthread_mutex_unlock(ptrarg->mutin);
+	return NULL;
 }
 
 
@@ -38,27 +58,53 @@ int main(int argc, char **argv)
 	if(argc > 1)
 	{
 		int i;
-		arg_t arg;
-		nbThreads = atoi(argv[1]);
+		int nbThreads = atoi(argv[1]);
+		arg_t *arg = malloc(sizeof(arg_t)*nbThreads);
 		pthread_t id[nbThreads];
 		pthread_mutex_t mut[nbThreads];
 		pthread_cond_t cond[nbThreads];
-		arg.nbFinish = 0;
-		pthread_mutex_init(&arg.mut, NULL);
-		pthread_cond_init(&arg.cond, NULL);
+		int values[nbThreads];
 		
-		srand(getpid());
-		pthread_create(id, NULL, ft_thread, &arg);
-		pthread_join(id[0], NULL);
-		
-		for(i=1; i<bar.nbThreads-1; i++)
+		/*Init con et mut*/
+		for(i=0;i<nbThreads;i++)
 		{
-			pthread_create(id+i, NULL, ft_thread, &arg);
-			pthread_join(id[i], NULL);
+			pthread_mutex_init(mut+i, NULL);
+			pthread_cond_init(cond+i, NULL);
 		}
 		
-		pthread_create(id+nbThreads-1, NULL, ft_thread, &arg);
-		pthread_join(id[nbThreads-1], NULL);	
+		/*Init tab args*/
+		arg[0].numThread = 0;
+		arg[0].out = values+1;
+		arg[0].mutout = mut;
+		arg[0].condout = cond;
+		for(i=1;i<nbThreads-1;i++)
+		{
+			arg[i].numThread = i;
+			arg[i].in = values+i;
+			arg[i].out = values+(i+1);
+			arg[i].mutin = mut+(i-1);
+			arg[i].mutout = mut+i;
+			arg[i].condin = cond+(i-1);
+			arg[i].condout = cond+i;
+		}
+		arg[i].numThread = i;
+		arg[i].in = values+i;
+		arg[i].mutin = mut+(i-1);
+		arg[i].condin = cond+(i-1);
+		/*Fin init args*/
+		
+		srand(getpid());
+		
+		/*Create*/
+		for(i=1; i<nbThreads-1; i++)
+			pthread_create(id+i, NULL, ft_thread_mid, &arg[i]);
+		pthread_create(id+nbThreads-1, NULL, ft_thread_end, &arg[nbThreads-1]);
+		sleep(1);
+		pthread_create(id, NULL, ft_thread_first, &arg[0]);
+		/*Join*/
+		for(i=0; i<nbThreads; i++)
+			pthread_join(id[i], NULL);
+		free(arg);
 		return 0;
 	}
 }
